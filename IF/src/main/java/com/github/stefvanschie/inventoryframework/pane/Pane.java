ab62730536e1443ab8a65d89a1ab7758a5d55de4
hsgamer <huynhqtienvtag@gmail.com>
2021-05-21 10:42:02 +0700

    version-based EnchantmentUtil

--- IF/src/main/java/com/github/stefvanschie/inventoryframework/pane/Pane.java
+++ IF/src/main/java/com/github/stefvanschie/inventoryframework/pane/Pane.java
@@ -5,6 +5,7 @@ import com.github.stefvanschie.inventoryframework.exception.XMLReflectionExcepti
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.InventoryComponent;
 import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
+import com.github.stefvanschie.inventoryframework.util.EnchantmentUtil;
 import com.github.stefvanschie.inventoryframework.util.SkullUtil;
 import com.github.stefvanschie.inventoryframework.util.UUIDMetaUtil;
 import com.github.stefvanschie.inventoryframework.util.XMLUtil;
@@ -368,9 +369,7 @@ public abstract class Pane {
                                 if (!innerNode.getNodeName().equals("enchantment"))
                                     continue;
 
-                                Enchantment enchantment = Enchantment.getByName(
-                                    innerElementChild.getAttribute("id").toUpperCase(Locale.getDefault())
-                                );
+                                Enchantment enchantment = EnchantmentUtil.getEnchantment(innerElementChild.getAttribute("id"));
 
                                 if (enchantment == null) {
                                     throw new XMLLoadException("Enchantment cannot be found");
@@ -485,7 +484,7 @@ public abstract class Pane {
                 throw new XMLLoadException(exception);
             }
         }
-		
+
 		item.setProperties(properties);
 
         return item;
@@ -609,7 +608,7 @@ public abstract class Pane {
     public void setOnClick(@Nullable Consumer<InventoryClickEvent> onClick) {
         this.onClick = onClick;
     }
-    
+
     /**
      * Calls the consumer (if it's not null) that was specified using {@link #setOnClick(Consumer)},
      * so the consumer that should be called whenever this pane is clicked in.
@@ -622,7 +621,7 @@ public abstract class Pane {
         if (onClick == null) {
             return;
         }
-    
+
         try {
             onClick.accept(event);
         } catch (Throwable t) {
@@ -647,7 +646,7 @@ public abstract class Pane {
         if (PROPERTY_MAPPINGS.containsKey(attributeName)) {
             throw new IllegalArgumentException("property '" + attributeName + "' is already registered");
         }
-    
+
         PROPERTY_MAPPINGS.put(attributeName, function);
     }
 
